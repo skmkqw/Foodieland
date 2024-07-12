6a3ab38d7a61665055269f54d486d426f2b29937
skmkqw <tkorsakov77@gmail.com>
2024-07-12 23:37:10 +0200

    Displaying validation and invalid credentials errors

--- backend/foodieland/obj/Debug/net8.0/foodieland.AssemblyInfo.cs
+++ backend/foodieland/obj/Debug/net8.0/foodieland.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("foodieland")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b0a8367937364a316123c2d67d6a0abdcb2daf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aad615ae3aec8c25ccc04afcef7dee31b645544a")]
 [assembly: System.Reflection.AssemblyProductAttribute("foodieland")]
 [assembly: System.Reflection.AssemblyTitleAttribute("foodieland")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
