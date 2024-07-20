2499ecd99f23fb7f1f64c6163d3aab912485a8d6
skmkqw <tkorsakov77@gmail.com>
2024-07-20 23:38:19 +0200

    create/update session methods

---
+++ frontend/foodieland-app/lib/session.ts
@@ -0,0 +1,16 @@
+import { cookies } from "next/headers";
+
+const SESSION_DURATION_MS = 10 * 1000;
+
+export function createSession(token: string) {
+    const expires = new Date(Date.now() + SESSION_DURATION_MS);
+    cookies().set("session", token, { httpOnly: true, secure: true, expires: expires });
+}
+
+export function updateSession() {
+    const session = cookies().get("session")?.value;
+    if (session) {
+        const expires = new Date(Date.now() + SESSION_DURATION_MS);
+        cookies().set('session', session, { httpOnly: true, secure: true, expires: expires });
+    }
+}
