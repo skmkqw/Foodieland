c10d781e306127c87703c3e8cee461b47748359c
skmkqw <tkorsakov77@gmail.com>
2024-07-20 23:45:38 +0200

    updated signupSchema

--- frontend/foodieland-app/actions/auth.ts
+++ frontend/foodieland-app/actions/auth.ts
@@ -1,20 +1,32 @@
 'use server'
 
 import { z } from 'zod';
-import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { axiosInstance } from "@/lib/axios";
 import { createSession } from "@/lib/session";
 
+
 const signupSchema = z.object({
-    email: z.string().email({ message: 'Invalid email address' }),
-    password: z.string().min(8, { message: 'Password must be at least 8 characters long' })
+    firstName: z.string()
+        .trim()  // Remove leading and trailing whitespace
+        .min(1, { message: 'First name is required and cannot be empty' }),
+
+    lastName: z.string()
+        .trim()  // Remove leading and trailing whitespace
+        .min(1, { message: 'Last name is required and cannot be empty' }),
+
+    email: z.string()
+        .email({ message: 'Invalid email address' }),
+
+    password: z.string()
+        .min(8, { message: 'Password must be at least 8 characters long' })
         .regex(/(?=.*[A-Z])/, { message: 'Password must contain at least one capital letter' })
         .regex(/(?=.*[!@#$%^&*])/, { message: 'Password must contain at least one special character' }),
 });
 
 const loginSchema = z.object({
    email: z.string().email({ message: 'Invalid email address' }),
+
    password: z.string().min(8, { message: 'Password must be at least 8 characters long' })
 });
 
