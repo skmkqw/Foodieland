a7d46ba745710f7c7ed6b3d3639bbd917da1225d
skmkqw <tkorsakov77@gmail.com>
2024-07-21 00:15:35 +0200

    session methods as server actions

--- frontend/foodieland-app/lib/session.ts
+++ frontend/foodieland-app/lib/session.ts
@@ -1,13 +1,15 @@
+'use server'
+
 import { cookies } from "next/headers";
 
 const SESSION_DURATION_MS = 10 * 1000;
 
-export function createSession(token: string) {
+export async function createSession(token: string) {
     const expires = new Date(Date.now() + SESSION_DURATION_MS);
     cookies().set("session", token, { httpOnly: true, secure: true, expires: expires });
 }
 
-export function updateSession() {
+export async function updateSession() {
     const session = cookies().get("session")?.value;
     if (session) {
         const expires = new Date(Date.now() + SESSION_DURATION_MS);
