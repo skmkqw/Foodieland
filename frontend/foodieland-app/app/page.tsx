b4e5eab587e3908cd853b741f629a0162d22d0bb
skmkqw <tkorsakov77@gmail.com>
2024-07-21 18:36:47 +0200

    Displaying user data on main page

--- frontend/foodieland-app/app/page.tsx
+++ frontend/foodieland-app/app/page.tsx
@@ -1,9 +1,14 @@
-export default function Home() {
-  return (
+import {getUserData} from "@/lib/authorization";
+
+export default async function Home() {
+    const userData = await getUserData();
+    return (
     <main>
       <div className='container'>
           <h1>FoodieLand</h1>
+          <p>{userData?.unique_name}</p>
+          <p>{userData?.email}</p>
       </div>
     </main>
-  );
+    );
 }
