465aca88c362d9c8777f6fc6f5b502e710df0913
skmkqw <tkorsakov77@gmail.com>
2024-07-21 18:56:59 +0200

    using logout action on main page

--- frontend/foodieland-app/app/page.tsx
+++ frontend/foodieland-app/app/page.tsx
@@ -1,14 +1,33 @@
 import {getUserData} from "@/lib/authorization";
+import {Button} from "@/components";
+import {logout} from "@/actions/auth";
+import {redirect} from "next/navigation";
+import Link from "next/link";
 
 export default async function Home() {
     const userData = await getUserData();
     return (
     <main>
-      <div className='container'>
-          <h1>FoodieLand</h1>
-          <p>{userData?.unique_name}</p>
-          <p>{userData?.email}</p>
-      </div>
+        <div className='container'>
+            <h1>FoodieLand</h1>
+            {userData ? (
+                <div>
+                    <p>{userData?.unique_name}</p>
+                    <p>{userData?.email}</p>
+                    <form action={logout}>
+                        <Button type="submit" text="Log out"/>
+                    </form>
+                </div>
+                ) : (
+                <div>
+                    <p>You are not logged in.</p>
+                    <Link href='http://localhost:3000/login'>
+                        <Button type="button" text="Log in"/>
+                    </Link>
+                </div>
+            )}
+        </div>
     </main>
-    );
+)
+    ;
 }
