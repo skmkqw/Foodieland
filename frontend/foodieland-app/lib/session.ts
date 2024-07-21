58f590b86bcfa47e10d15aaab653be0eb12443df
skmkqw <tkorsakov77@gmail.com>
2024-07-21 17:36:45 +0200

    fixed updateSession method

--- frontend/foodieland-app/lib/session.ts
+++ frontend/foodieland-app/lib/session.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { cookies } from "next/headers";
+import {NextRequest, NextResponse} from "next/server";
 
 const SESSION_DURATION_MS = 10 * 1000;
 
@@ -9,10 +10,13 @@ export async function createSession(token: string) {
     cookies().set("session", token, { httpOnly: true, secure: true, expires: expires });
 }
 
-export async function updateSession() {
-    const session = cookies().get("session")?.value;
-    if (session) {
-        const expires = new Date(Date.now() + SESSION_DURATION_MS);
-        cookies().set('session', session, { httpOnly: true, secure: true, expires: expires });
-    }
+export async function updateSession(request: NextRequest) {
+    const session = request.cookies.get("session")?.value;
+    if (!session) return;
+
+    const expires = new Date(Date.now() + SESSION_DURATION_MS);
+    const response = NextResponse.next();
+    response.cookies.set('session', session, { httpOnly: true, secure: true, expires: expires });
+
+    return response;
 }
--- frontend/foodieland-app/middleware.ts
+++ frontend/foodieland-app/middleware.ts
@@ -1,10 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import {updateSession} from "@/lib/session";
 
-export function middleware(request: NextRequest) {
-    const response = NextResponse.next();
-    updateSession();
-    return response;
+export async function middleware(request: NextRequest) {
+    return await updateSession(request);
 }
 export const config = {
     matcher: ['/((?!api|_next/static|favicon.ico).*)'],
