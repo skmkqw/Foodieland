8f191cc4d9a8d34883bb9a8ef1ac486b52816fad
skmkqw <tkorsakov77@gmail.com>
2024-07-21 20:36:47 +0200

    override breakpoints in talwind config

--- frontend/foodieland-app/tailwind.config.js
+++ frontend/foodieland-app/tailwind.config.js
@@ -6,7 +6,21 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '320px',
+
+        'sm': '480px',
+
+        'md': '768px',
+
+        'lg': '1024px',
+
+        'xl': '1280px',
+
+        '2xl': '1536px'
+      }
+    },
   },
   plugins: [],
 }
