574e93c81e57d120c64dd5a8cd3d6af395d26c31
skmkqw <tkorsakov77@gmail.com>
2024-07-22 21:11:00 +0200

    Using InputGroup in login form

--- frontend/foodieland-app/components/forms/LoginForm.tsx
+++ frontend/foodieland-app/components/forms/LoginForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button } from "@/components";
+import {InputGroup, Button} from "@/components";
 import { login } from "@/actions/auth";
 import  { useFormState } from "react-dom";
 export default function LoginForm() {
@@ -8,38 +8,12 @@ export default function LoginForm() {
 
     return (
         <form action={formAction}>
-            <div className="inputGroup">
-                <label htmlFor="email">E-mail</label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="example@email.com"
-                    required
-                />
-                {errorMessage?.errors.email && (
-                    <p className="errorMessage">{errorMessage.errors.email}</p>
-                )}
-            </div>
-
-            <div className="inputGroup">
-                <label htmlFor="password">Password</label>
-                <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder="********"
-                    required
-                />
-                {errorMessage?.errors.password && (
-                    <p className="errorMessage">{errorMessage.errors.password}</p>
-                )}
-                {errorMessage?.errors.general && (
-                    <p className="errorMessage">{errorMessage.errors.general}</p>
-                )}
-            </div>
-
-            <Button type={'submit'} additionalStyles="w-full" text={'Log in'}/>
+            <InputGroup type="email" errorMessage={errorMessage?.errors.email} />
+            <InputGroup type="password" errorMessage={errorMessage?.errors.password} />
+            {errorMessage?.errors.general && (
+                <p className="errorMessage mt-2">{errorMessage.errors.general}</p>
+            )}
+            <Button type={'submit'} additionalStyles="w-full mt-5" text={'Log in'}/>
         </form>
     );
 }
