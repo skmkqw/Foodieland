fb1077cf9cd633f26c0042abfc69ad54e0f58c05
skmkqw <tkorsakov77@gmail.com>
2024-07-22 21:10:22 +0200

    InputGroup component

---
+++ frontend/foodieland-app/components/InputGroup.tsx
@@ -0,0 +1,58 @@
+export default function InputGroup({ type, errorMessage } : { type: "email" | "username" | "password", errorMessage?: string }) {
+    let inputGroup;
+
+    switch (type) {
+        case "email":
+            inputGroup = (
+                <div className="inputGroup">
+                    <label htmlFor="email">E-mail</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="example@email.com"
+                        required
+                    />
+                </div>
+            );
+            break;
+        case "password":
+            inputGroup = (
+                <div className="inputGroup">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="********"
+                        required
+                    />
+                </div>
+            );
+            break;
+        case "username":
+            inputGroup = (
+                <div className="inputGroup">
+                    <label htmlFor="username">E-mail</label>
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        placeholder="John Doe"
+                        required
+                    />
+                </div>
+            );
+            break;
+        default:
+            inputGroup = null;
+            break;
+    }
+
+    return (
+        <div>
+            {inputGroup}
+            {errorMessage && <p className="errorMessage mt-2">{errorMessage}</p>}
+        </div>
+    );
+}
--- frontend/foodieland-app/components/index.ts
+++ frontend/foodieland-app/components/index.ts
@@ -1,6 +1,7 @@
 import Logo from "@/components/Logo";
 import Button from "@/components/Button";
 import LinkButton from "@/components/LinkButton";
+import InputGroup from "@/components/InputGroup";
 import Promosection from "@/components/Promosection";
 import LoginForm from "@/components/forms/LoginForm";
 import RegisterForm from "@/components/forms/RegisterForm";
@@ -12,4 +13,5 @@ export {
   LinkButton,
   RegisterForm,
   Promosection,
+  InputGroup
 };
