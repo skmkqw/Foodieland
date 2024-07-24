71718bdae9f0ae0ba2a3c2e5a99c754b20546544
skmkqw <tkorsakov77@gmail.com>
2024-07-24 16:01:29 +0200

    used InputGroups in RegisterForm

--- frontend/foodieland-app/components/forms/RegisterForm.tsx
+++ frontend/foodieland-app/components/forms/RegisterForm.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import {Button} from "@/components";
+import {Button, InputGroup} from "@/components";
 import {useFormState} from "react-dom";
 import {signup} from "@/actions/auth";
+
 export default function RegisterForm()
 {
     const [errorMessage, formAction, isPending] = useFormState(signup, undefined,);
@@ -20,59 +21,12 @@ export default function RegisterForm()
 
     return (
         <form action={formAction}>
-            <div className="inputGroup">
-                <label htmlFor="firstName">Full name</label>
-                <input
-                    type="text"
-                    id="firstName"
-                    name="firstName"
-                    placeholder="John"
-                    required
-                />
-                {errorMessage?.errors.firstName && (
-                    <p className="errorMessage">{errorMessage.errors.firstName}</p>
-                )}
-            </div>
-            <div className="inputGroup">
-                <label htmlFor="lastName">Full name</label>
-                <input
-                    type="text"
-                    id="lastName"
-                    name="lastName"
-                    placeholder="Doe"
-                    required
-                />
-                {errorMessage?.errors.lastName && (
-                    <p className="errorMessage">{errorMessage.errors.lastName}</p>
-                )}
-            </div>
-            <div className="inputGroup">
-                <label htmlFor="email">E-mail</label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="example@email.com"
-                    required
-                />
-                {errorMessage?.errors.email && (
-                    <p className="errorMessage">{errorMessage.errors.email}</p>
-                )}
-            </div>
-            <div className="inputGroup">
-                <label htmlFor="password">Password</label>
-                <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder="********"
-                    required
-                />
-                {errorMessage?.errors.password && formatErrors(errorMessage.errors.password)}
-                {errorMessage?.errors.general && (
-                    <p className="errorMessage">{errorMessage.errors.general}</p>
-                )}
-            </div>
+            <InputGroup type="username" errorMessage={errorMessage?.errors.fullName} />
+            <InputGroup type="email" errorMessage={errorMessage?.errors.email} />
+            <InputGroup type="password" errorMessage={errorMessage?.errors.password && formatErrors(errorMessage.errors.password)} />
+            {errorMessage?.errors.general && (
+                <p className="errorMessage">{errorMessage.errors.general}</p>
+            )}
             <Button type={'submit'} additionalStyles="w-full mt-5" text={'Sign up'}/>
         </form>
     );
