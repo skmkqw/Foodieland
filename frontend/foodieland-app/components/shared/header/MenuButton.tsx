c9b733b2439c99dd7d2a091dabdbdb4ccc4a55a1
skmkqw <tkorsakov77@gmail.com>
2024-07-26 18:44:47 +0200

    Fixed MenuButton

--- frontend/foodieland-app/components/shared/header/MenuButton.tsx
+++ frontend/foodieland-app/components/shared/header/MenuButton.tsx
@@ -3,16 +3,16 @@ export default function MenuButton({ onClick, isOpen }: { onClick: () => void, i
         <button onClick={onClick}
                 className="flex flex-col justify-center items-center z-20">
             <span className={`bg-black block transition-all duration-300 ease-out 
-                            h-0.5 w-6 rounded-sm ${isOpen ?
-                "rotate-45 translate-y-1" : "-translate-y-0.5"
+                            h-[2px] w-6 rounded-sm ${isOpen ?
+                "rotate-45 translate-y-[4px]" : "-translate-y-0.5"
             }`} />
             <span className={`bg-black block transition-all duration-300 ease-out 
-                    h-0.5 w-6 rounded-sm my-0.5 ${isOpen ?
+                    h-[2px] w-6 my-0.5 rounded-sm ${isOpen ?
                 "opacity-0" : "opacity-100"
             }`} />
             <span className={`bg-black block transition-all duration-300 ease-out
-                        h-0.5 w-6 rounded-sm ${isOpen ?
-                "-rotate-45 -translate-y-1" : "translate-y-0.5"
+                        h-[2px] w-6 rounded-sm ${isOpen ?
+                "-rotate-45 -translate-y-[4px]" : "translate-y-0.5"
             }`} />
         </button>
     );
