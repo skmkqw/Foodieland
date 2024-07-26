5b648bfae80d9ab0e41ac51e6baf3e70d5135e83
skmkqw <tkorsakov77@gmail.com>
2024-07-26 15:45:26 +0200

    New social media icons

--- frontend/foodieland-app/components/header/SocialLinks.tsx
+++ frontend/foodieland-app/components/header/SocialLinks.tsx
@@ -5,10 +5,10 @@ export default function SocialLinks() {
     return (
         <div className="mt-8 md:mt-0 flex items-center gap-9">
             <Link href="#">
-                <Image src="/facebook.svg" alt="logo" height={20} width={9} />
+                <Image src="/facebook.svg" alt="logo" height={24} width={24} />
             </Link>
             <Link href="#">
-                <Image src="/twitter.svg" alt="logo" height={22} width={22} />
+                <Image src="/x.svg" alt="logo" height={22} width={22} />
             </Link>
             <Link href="#">
                 <Image src="/instagram.svg" alt="logo" height={22} width={22} />
