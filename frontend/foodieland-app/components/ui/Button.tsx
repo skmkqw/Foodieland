3deef7c04243b94f60c86133c525886cc08c4661
skmkqw <tkorsakov77@gmail.com>
2024-07-27 17:04:44 +0200

    passing children to Button/LinkButton

--- frontend/foodieland-app/components/ui/Button.tsx
+++ frontend/foodieland-app/components/ui/Button.tsx
@@ -6,14 +6,16 @@ interface ButtonProps {
     type: "button" | "submit" | "reset",
     text: string,
     additionalStyles?: string,
-    handleClick?: MouseEventHandler<HTMLButtonElement>
+    handleClick?: MouseEventHandler<HTMLButtonElement>,
+    children?: React.ReactNode
 }
 
-export default function Button({ type, text, additionalStyles, handleClick }: ButtonProps) {
+export default function Button({ type, text, additionalStyles, handleClick, children }: ButtonProps) {
     return (
         <button type={type} onClick={handleClick}
-                className={`${additionalStyles} bg-black text-white font-medium text-lg transition-[background-color] duration-[0.3s] ease-[ease] cursor-pointer px-10 py-[15px] rounded-2xl hover:bg-[rgba(0,0,0,0.8)]`}>
-            {text}
+                className={`${additionalStyles} flex items-center gap-4 bg-black text-white font-medium text-lg transition-[background-color] duration-[0.3s] ease-[ease] cursor-pointer px-10 py-[15px] rounded-2xl hover:bg-[rgba(0,0,0,0.8)]`}>
+            <p>{text}</p>
+            {children}
         </button>
     );
 }
--- frontend/foodieland-app/components/ui/LinkButton.tsx
+++ frontend/foodieland-app/components/ui/LinkButton.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 import { Button } from "@/components";
 
-export default function LinkButton({ url, buttonText, additionalStyles }: { url: string, buttonText: string, additionalStyles?: string }) {
+export default function LinkButton({ url, buttonText, additionalStyles, children }: { url: string, buttonText: string, additionalStyles?: string, children?: React.ReactNode }) {
     return (
         <Link href={url}>
-            <Button type={"button"} text={buttonText} additionalStyles={additionalStyles}/>
+            <Button type={"button"} text={buttonText} additionalStyles={additionalStyles} children={children}/>
         </Link>
     );
 }
