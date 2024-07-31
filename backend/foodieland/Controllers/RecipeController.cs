eb6789e19126350aba1c2a0030319047c2b94dcd
skmkqw <tkorsakov77@gmail.com>
2024-08-01 00:55:11 +0200

    TryDetermineUser method

--- backend/foodieland/Controllers/RecipeController.cs
+++ backend/foodieland/Controllers/RecipeController.cs
@@ -303,4 +303,26 @@ public class RecipeController : ControllerBase
         } 
         return NotFound("Recipe not found");
     }
+    
+    private async Task<AppUser?> TryDetermineUser(string? authorizationHeader)
+    {
+        if (authorizationHeader != null && authorizationHeader.StartsWith("Bearer "))
+        {
+            var token = authorizationHeader.Substring("Bearer ".Length).Trim();
+            var handler = new JwtSecurityTokenHandler();
+
+            if (handler.ReadToken(token) is JwtSecurityToken jwtToken)
+            {
+                var userIdClaim = jwtToken.Claims.FirstOrDefault(claim => claim.Type == "nameid");
+                if (userIdClaim != null)
+                {
+                    var userId = userIdClaim.Value;
+                    
+                    return await _userManager.FindByIdAsync(userId);
+                }
+            }
+        }
+
+        return null;
+    }
 }
