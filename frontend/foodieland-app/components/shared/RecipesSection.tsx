5dbfc1c6cfc9821860a802dde7a7ec5146231c92
skmkqw <tkorsakov77@gmail.com>
2024-08-01 19:12:07 +0200

    RecipeCard props

--- frontend/foodieland-app/components/shared/RecipesSection.tsx
+++ frontend/foodieland-app/components/shared/RecipesSection.tsx
@@ -1,14 +1,73 @@
 import { Container, Description, RecipeCard, Title } from "@/components";
 
+const recipeCards = [
+    {
+        imagePath: "/featured-recipe.jpg",
+        name: "Lorem ipsum dolor sit amet",
+        category: "Meat",
+        timeToCook: 30
+    },
+    {
+        imagePath: "/featured-recipe.jpg",
+        name: "Lorem ipsum dolor sit amet",
+        category: "Vegan",
+        timeToCook: 20
+    },
+    {
+        imagePath: "/featured-recipe.jpg",
+        name: "Lorem ipsum dolor sit amet",
+        category: "Lunch",
+        timeToCook: 10
+    },
+    {
+        imagePath: "/featured-recipe.jpg",
+        name: "Lorem ipsum dolor sit amet",
+        category: "Meat",
+        timeToCook: 30
+    },
+    {
+        imagePath: "/featured-recipe.jpg",
+        name: "Lorem ipsum dolor sit amet",
+        category: "Vegan",
+        timeToCook: 20
+    },
+    {
+        imagePath: "/featured-recipe.jpg",
+        name: "Lorem ipsum dolor sit amet",
+        category: "Lunch",
+        timeToCook: 10
+    },
+    {
+        imagePath: "/featured-recipe.jpg",
+        name: "Lorem ipsum dolor sit amet",
+        category: "Meat",
+        timeToCook: 30
+    },
+    {
+        imagePath: "/featured-recipe.jpg",
+        name: "Lorem ipsum dolor sit amet",
+        category: "Vegan",
+        timeToCook: 20
+    },
+    {
+        imagePath: "/featured-recipe.jpg",
+        name: "Lorem ipsum dolor sit amet",
+        category: "Lunch",
+        timeToCook: 10
+    },
+];
+
 export default function RecipesSection() {
     return (
         <Container className="w-full flex flex-col items-center gap-10 text-center">
             <Title text="Simple and tasty recipes" />
-            <Description text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut enim libero neque non quas repellat repellendus tenetur ullam voluptates?" className="max-w-2xl" />
+            <Description
+                text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut enim libero neque non quas repellat repellendus tenetur ullam voluptates?"
+                className="max-w-2xl" />
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-                <RecipeCard />
-                <RecipeCard />
-                <RecipeCard />
+                {recipeCards.map((recipe, idx) => (
+                    <RecipeCard {...recipe} key={idx} />
+                ))}
             </div>
         </Container>
     );
--- frontend/foodieland-app/components/shared/cards/RecipeCard.tsx
+++ frontend/foodieland-app/components/shared/cards/RecipeCard.tsx
@@ -1,19 +1,26 @@
 import Image from "next/image";
 
-export default function RecipeCard () {
+interface RecipeCardProps {
+    imagePath: string,
+    name: string,
+    category: string,
+    timeToCook: number
+}
+
+export default function RecipeCard({ imagePath, name, category, timeToCook }: RecipeCardProps) {
     return (
         <div className="p-4 flex flex-col gap-7 bg-gradient-to-b from-white to-primary rounded-3xl">
-            <Image src="/featured-recipe.jpg" alt="Recipe image" width={370} height={250} className="rounded-3xl" />
+            <Image src={imagePath} alt="Recipe image" width={370} height={250} className="rounded-3xl" />
             <div className="flex flex-col gap-5 px-3">
-                <p className="font-semibold text-3xl text-start">Lorem ipsum dolor sit dolor sit.</p>
+                <p className="font-semibold text-3xl text-start">{name}</p>
                 <div className="flex items-center gap-5 pb-9">
                     <div className="flex items-center gap-3">
                         <Image src="/timer.svg" alt="Fork and knife" width={24} height={24} />
-                        <p className="text-base text-black text-opacity-60 font-medium">30 Minutes</p>
+                        <p className="text-base text-black text-opacity-60 font-medium">{timeToCook} Minutes</p>
                     </div>
                     <div className="flex items-center gap-3">
                         <Image src="/fork-knife.svg" alt="Fork and knife" width={24} height={24} />
-                        <p className="text-base text-black text-opacity-60 font-medium">Meat</p>
+                        <p className="text-base text-black text-opacity-60 font-medium">{category}</p>
                     </div>
                 </div>
             </div>
