56caeaee73a8e489e1dfaa0d53cbfc9b10f5fd7a
skmkqw <tkorsakov77@gmail.com>
2024-08-02 21:02:14 +0200

    Common directory

---
+++ backend/foodieland/Common/MeasurementUnits.cs
@@ -0,0 +1,72 @@
+namespace foodieland.Common;
+
+public enum MeasurementUnits
+{
+    // Volume
+    Teaspoon,
+    Tablespoon,
+    Cup,
+    Milliliter,
+    Liter,
+    FluidOunce,
+    Pint,
+    Quart,
+    Gallon,
+
+    // Weight
+    Gram,
+    Kilogram,
+    Milligram,
+    Ounce,
+    Pound,
+    Stone,
+    Ton,
+
+    // Length
+    Millimeter,
+    Centimeter,
+    Meter,
+    Inch,
+    Foot,
+    
+
+    // Miscellaneous
+    Pinch,
+    Dash,
+    Drop,
+    Handful,
+
+    // International units
+    Deciliter,
+    Centiliter,
+    Decagram,
+    Hectogram,
+
+    // Cooking specific
+    Clove,
+    Slice,
+    Piece,
+    Fillet,
+    Stick,
+    Can,
+    Jar,
+    Bottle,
+    Packet,
+    Bunch,
+    Head,
+    Leaf,
+    Sprig,
+    Stalk,
+    Strip,
+
+    // Liquid specific
+    Barrel,
+    Hogshead,
+    Gill,
+    Firkin,
+
+    // Baking specific
+    Sheet,
+    Loaf,
+    Scoop
+}
---
+++ backend/foodieland/Common/RecipeCategories.cs
@@ -0,0 +1,15 @@
+namespace foodieland.Common;
+
+public enum RecipeCategories
+{
+    Vegan,
+    Breakfast,
+    Lunch,
+    Meat,
+    Chicken,
+    Fish,
+    Diet,
+    Dessert,
+    Chocolate,
+    Asian
+}
