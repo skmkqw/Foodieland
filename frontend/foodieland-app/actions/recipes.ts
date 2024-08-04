a4d96e66d822c5d47aa72c6fea203ec8d0ff711c
skmkqw <tkorsakov77@gmail.com>
2024-08-04 04:03:53 +0200

    fetchRecipes action

--- frontend/foodieland-app/actions/recipes.ts
+++ frontend/foodieland-app/actions/recipes.ts
@@ -1,5 +1,22 @@
 "use server";
 
-export async function fetchRecipes(recipeAmount: number) {
+import { z } from "zod";
+import { axiosInstance } from "@/lib/axios";
+import { RecipeProps, recipeSchema } from "@/schemas/recipe";
 
-}
+const featuredRecipesSchema = z.array(recipeSchema);
+
+export const fetchRecipes = async (recipeAmount: number): Promise<RecipeProps[] | undefined> => {
+    try {
+        const response = await axiosInstance.get(`/recipes/featured?page=1&pageSize=${recipeAmount}`);
+        const parsedData = featuredRecipesSchema.safeParse(response.data);
+
+        if (!parsedData.success) {
+            console.error("Invalid data structure:", parsedData.error);
+        }
+
+        return parsedData.data;
+    } catch (error) {
+        console.error("Error fetching recipes:", error);
+    }
+};
