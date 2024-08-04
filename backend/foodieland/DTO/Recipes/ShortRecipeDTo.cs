7a7397d8c8db5523d2e7a7cb49d7bb0a5d5579c4
skmkqw <tkorsakov77@gmail.com>
2024-08-04 04:00:28 +0200

    ShortRecipeDTo

---
+++ backend/foodieland/DTO/Recipes/ShortRecipeDTo.cs
@@ -0,0 +1,16 @@
+using foodieland.Models;
+
+namespace foodieland.DTO.Recipes;
+
+public class ShortRecipeDTo
+{
+    public string Name { get; set; } = string.Empty;
+    
+    public string Description { get; set; } = string.Empty;
+    
+    public uint TimeToCook { get; set; }
+    
+    public RecipeCategories Category { get; set; }
+    
+    public bool IsLiked { get; set; }
+}
