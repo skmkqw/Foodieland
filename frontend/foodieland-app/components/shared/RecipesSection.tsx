41fbc164b480dee9f8562a485630aebd66880e9d
skmkqw <tkorsakov77@gmail.com>
2024-08-04 04:13:18 +0200

    utilized new server action

--- frontend/foodieland-app/components/shared/RecipesSection.tsx
+++ frontend/foodieland-app/components/shared/RecipesSection.tsx
@@ -1,63 +1,11 @@
 import { Container, Description, RecipeCard, Title } from "@/components";
+import { fetchRecipes } from "@/actions/recipes";
 
-const recipeCards = [
-    {
-        imagePath: "/featured-recipe.jpg",
-        name: "Lorem ipsum dolor sit amet",
-        category: "Meat",
-        timeToCook: 30
-    },
-    {
-        imagePath: "/featured-recipe.jpg",
-        name: "Lorem ipsum dolor sit amet",
-        category: "Vegan",
-        timeToCook: 20
-    },
-    {
-        imagePath: "/featured-recipe.jpg",
-        name: "Lorem ipsum dolor sit amet",
-        category: "Lunch",
-        timeToCook: 10
-    },
-    {
-        imagePath: "/featured-recipe.jpg",
-        name: "Lorem ipsum dolor sit amet",
-        category: "Meat",
-        timeToCook: 30
-    },
-    {
-        imagePath: "/featured-recipe.jpg",
-        name: "Lorem ipsum dolor sit amet",
-        category: "Vegan",
-        timeToCook: 20
-    },
-    {
-        imagePath: "/featured-recipe.jpg",
-        name: "Lorem ipsum dolor sit amet",
-        category: "Lunch",
-        timeToCook: 10
-    },
-    {
-        imagePath: "/featured-recipe.jpg",
-        name: "Lorem ipsum dolor sit amet",
-        category: "Meat",
-        timeToCook: 30
-    },
-    {
-        imagePath: "/featured-recipe.jpg",
-        name: "Lorem ipsum dolor sit amet",
-        category: "Vegan",
-        timeToCook: 20
-    },
-    {
-        imagePath: "/featured-recipe.jpg",
-        name: "Lorem ipsum dolor sit amet",
-        category: "Lunch",
-        timeToCook: 10
-    }
-];
 
-export default function RecipesSection() {
+export default async function RecipesSection() {
+    const recipes = await fetchRecipes(6);
+    if (!recipes) return <p>Error loading recipes</p>;
+
     return (
         <Container className="w-full flex flex-col items-center gap-10 text-center">
             <Title text="Simple and tasty recipes" />
@@ -65,8 +13,9 @@ export default function RecipesSection() {
                 text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut enim libero neque non quas repellat repellendus tenetur ullam voluptates?"
                 className="max-w-2xl" />
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-                {recipeCards.map((recipe, idx) => (
-                    <RecipeCard {...recipe} key={idx} />
+                {recipes.map((recipe, idx) => (
+                    <RecipeCard imagePath="/featured-recipe.jpg" name={recipe.name} category={recipe.category}
+                                timeToCook={recipe.timeToCook} key={idx} />
                 ))}
             </div>
         </Container>
