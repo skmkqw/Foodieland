dc646dfcead9604ce08d722bba61b1f3e20ee7de
skmkqw <tkorsakov77@gmail.com>
2024-08-05 22:13:43 +0200

    Updated RecipeCard & RecipeSection props

--- frontend/foodieland-app/components/shared/RecipesSection.tsx
+++ frontend/foodieland-app/components/shared/RecipesSection.tsx
@@ -23,7 +23,7 @@ export default async function RecipesSection() {
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
                 {recipes.map((recipe, idx) => (
                     <RecipeCard imagePath="/featured-recipe.jpg" name={recipe.name} category={recipe.category}
-                                timeToCook={recipe.timeToCook} key={idx} />
+                                timeToCook={recipe.timeToCook} isLiked={recipe.isLiked} key={idx} />
                 ))}
             </div>
         </Container>
--- frontend/foodieland-app/components/shared/cards/RecipeCard.tsx
+++ frontend/foodieland-app/components/shared/cards/RecipeCard.tsx
@@ -8,18 +8,19 @@ interface RecipeCardProps {
     imagePath: string,
     name: string,
     category: string,
-    timeToCook: number
+    timeToCook: number,
+    isLiked: boolean
 }
 
-export default function RecipeCard({ imagePath, name, category, timeToCook }: RecipeCardProps) {
-    const [isLiked, setIsLiked] = useState(false);
+export default function RecipeCard({ imagePath, name, category, timeToCook, isLiked }: RecipeCardProps) {
+    const [likeButtonActive, setLikeButtonActive] = useState(isLiked);
 
     const handleToggleLike = () => {
-        setIsLiked(!isLiked);
+        setLikeButtonActive(!likeButtonActive);
     };
     return (
         <div className="relative p-4 flex flex-col gap-7 bg-gradient-to-b from-white to-primary rounded-3xl">
-            <LikeButton isLiked={isLiked} onToggle={handleToggleLike} className="absolute top-[6%] right-[10%]" />
+            <LikeButton isLiked={likeButtonActive} onToggle={handleToggleLike} className="absolute top-[6%] right-[10%]" />
             <Image src={imagePath} alt="Recipe image" width={370} height={250}
                    className="rounded-3xl w-full object-cover" />
             <div className="flex flex-col gap-5 px-3">
