7afac70feb27978fe91ba285c5850a3f402808a8
skmkqw <tkorsakov77@gmail.com>
2024-08-06 20:29:48 +0200

    blocked page scrolling while in MobileDrawer

--- frontend/foodieland-app/components/shared/header/MenuButton.tsx
+++ frontend/foodieland-app/components/shared/header/MenuButton.tsx
@@ -1,17 +1,17 @@
-export default function MenuButton({ onClick, isOpen }: { onClick: () => void, isOpen: boolean }) {
+export default function MenuButton({ onClick, isActive }: { onClick: () => void, isActive: boolean }) {
     return (
         <button onClick={onClick}
                 className="flex flex-col justify-center items-center z-20">
             <span className={`bg-black block transition-all duration-300 ease-out 
-                            h-[2px] w-6 rounded-sm ${isOpen ?
+                            h-[2px] w-6 rounded-sm ${isActive ?
                 "rotate-45 translate-y-[4px]" : "-translate-y-0.5"
             }`} />
             <span className={`bg-black block transition-all duration-300 ease-out 
-                    h-[2px] w-6 my-0.5 rounded-sm ${isOpen ?
+                    h-[2px] w-6 my-0.5 rounded-sm ${isActive ?
                 "opacity-0" : "opacity-100"
             }`} />
             <span className={`bg-black block transition-all duration-300 ease-out
-                        h-[2px] w-6 rounded-sm ${isOpen ?
+                        h-[2px] w-6 rounded-sm ${isActive ?
                 "-rotate-45 -translate-y-[4px]" : "translate-y-0.5"
             }`} />
         </button>
