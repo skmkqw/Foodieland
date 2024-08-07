ddde8f469171dd1138668b80f72146a57d9418cd
skmkqw <tkorsakov77@gmail.com>
2024-08-07 22:53:34 +0200

    applied migrations

--- backend/foodieland/Migrations/20240807205228_UserImages.Designer.cs
+++ backend/foodieland/Migrations/20240807205228_UserImages.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using foodieland.Data;
 
@@ -11,9 +12,11 @@ using foodieland.Data;
 namespace foodieland.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240807205228_UserImages")]
+    partial class UserImages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/foodieland/Migrations/20240807205228_UserImages.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace foodieland.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ProfileImage",
+                table: "AspNetUsers",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfileImage",
+                table: "AspNetUsers");
+        }
+    }
+}
