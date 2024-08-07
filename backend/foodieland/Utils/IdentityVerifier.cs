df9303aaa4d4ef12de3b4798e586b0211579c4d8
skmkqw <tkorsakov77@gmail.com>
2024-08-07 22:45:54 +0200

    IdentityVerifier utility class

---
+++ backend/foodieland/Utils/IdentityVerifier.cs
@@ -0,0 +1,30 @@
+using System.IdentityModel.Tokens.Jwt;
+using foodieland.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace foodieland.Utils;
+
+public class IdentityVerifier
+{
+    public static async Task<AppUser?> TryDetermineUser(UserManager<AppUser> userManager, string? authorizationHeader)
+    {
+        if (authorizationHeader != null && authorizationHeader.StartsWith("Bearer "))
+        {
+            var token = authorizationHeader.Substring("Bearer ".Length).Trim();
+            var handler = new JwtSecurityTokenHandler();
+
+            if (handler.ReadToken(token) is JwtSecurityToken jwtToken)
+            {
+                var userIdClaim = jwtToken.Claims.FirstOrDefault(claim => claim.Type == "nameid");
+                if (userIdClaim != null)
+                {
+                    var userId = userIdClaim.Value;
+                    
+                    return await userManager.FindByIdAsync(userId);
+                }
+            }
+        }
+
+        return null;
+    }
+}
