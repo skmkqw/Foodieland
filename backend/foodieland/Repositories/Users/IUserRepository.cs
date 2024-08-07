ab45d4e54f231b37fda1329494b948672385914a
skmkqw <tkorsakov77@gmail.com>
2024-08-07 22:46:46 +0200

    GetById repo method

--- backend/foodieland/Repositories/Users/IUserRepository.cs
+++ backend/foodieland/Repositories/Users/IUserRepository.cs
@@ -4,5 +4,7 @@ namespace foodieland.Repositories.Users;
 
 public interface IUserRepository
 {
+    public Task<AppUser?> GetById(Guid userId);
+    
     public Task<AppUser?> AddImage(Guid userId, byte[] imageData);
 }
--- backend/foodieland/Repositories/Users/UserRepository.cs
+++ backend/foodieland/Repositories/Users/UserRepository.cs
@@ -1,27 +1,39 @@
 using foodieland.Data;
 using foodieland.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace foodieland.Repositories.Users;
 
 public class UserRepository : IUserRepository
 {
-    private readonly ApplicationDbContext _context;
+    private readonly UserManager<AppUser> _userManager;
 
-    public UserRepository(ApplicationDbContext context)
+    public UserRepository(UserManager<AppUser> userManager)
     {
-        _context = context;
+        _userManager = userManager;
     }
-    
+
+    public async Task<AppUser?> GetById(Guid userId)
+    {
+        return await _userManager.FindByIdAsync(userId.ToString());
+    }
+
     public async Task<AppUser?> AddImage(Guid userId, byte[] imageData)
     {
-        var user = await _context.Users.FindAsync(userId);
+        var user = await GetById(userId);
         if (user == null)
         {
             return null;
         }
 
         user.ProfileImage = imageData;
-        await _context.SaveChangesAsync();
-        return user;
+        var result = await _userManager.UpdateAsync(user);
+
+        if (result.Succeeded)
+        {
+            return user;
+        }
+
+        return null;
     }
 }
