4e5e13d1110557cd165a090252075c64d1af3c6d
skmkqw <tkorsakov77@gmail.com>
2024-08-07 21:57:40 +0200

    UserDto

---
+++ backend/foodieland/DTO/Users/UserDto.cs
@@ -0,0 +1,12 @@
+namespace foodieland.DTO.Users;
+
+public class UserDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    
+    public string LastName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+    
+    public string? ProfileImage { get; set; }
+}
