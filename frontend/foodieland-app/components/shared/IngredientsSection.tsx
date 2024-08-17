a8b45341fddae4f9230580c5bacb21111c6eeba7
skmkqw <tkorsakov77@gmail.com>
2024-08-18 00:06:10 +0200

    passing props to IngredientsSection

--- frontend/foodieland-app/components/shared/IngredientsSection.tsx
+++ frontend/foodieland-app/components/shared/IngredientsSection.tsx
@@ -1,15 +1,7 @@
 import { Container, IngredientList } from "@/components";
 import { Ingredient } from "@/types";
 
-const ingredients: Ingredient[] = [
-    { name: "Flour", amount: 2, unit: "cups" },
-    { name: "Sugar", amount: 0.5, unit: "cups" },
-    { name: "Butter", amount: 1, unit: "stick" },
-    { name: "Eggs", amount: 3, unit: "pieces" },
-    { name: "Vanilla Extract", amount: 1, unit: "teaspoon" }
-];
-
-export default function IngredientsSection() {
+export default function IngredientsSection({ ingredients }: { ingredients: Array<Ingredient> }) {
     return (
         <Container className="flex flex-col gap-5 md:gap-8 w-full mt-12 md:mt-20">
             <h1 className="font-semibold text-3xl md:text-4xl">Ingredients</h1>
