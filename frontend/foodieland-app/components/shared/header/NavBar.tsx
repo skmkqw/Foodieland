e67bd832730da951ebddea1bae5e83fbdc73abbc
skmkqw <tkorsakov77@gmail.com>
2024-08-17 20:52:31 +0200

    Logo link fix

--- frontend/foodieland-app/components/shared/header/NavBar.tsx
+++ frontend/foodieland-app/components/shared/header/NavBar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import Link from "next/link";
 import { Container, Logo, MenuButton, NavLinks, UserInfo } from "@/components";
 import MobileDrawer from "@/components/shared/header/MobileDrawer";
 
@@ -22,18 +21,14 @@ export default function NavBar({ userName }: { userName?: string }) {
         <>
             <div className="border-b-gray-200 border-b">
                 <Container className="hidden relative py-6 md:flex justify-between items-center gap-6">
-                    <Link href="/foodieland-app/public">
-                        <Logo fontsize={24} />
-                    </Link>
+                    <Logo fontsize={24} />
                     <NavLinks />
                     <UserInfo userName={userName} />
                 </Container>
             </div>
             <div className="border-b-gray-200 border-b">
                 <Container className="md:hidden relative flex items-center justify-between flex-row my-4">
-                    <Link href="/foodieland-app/public">
-                        <Logo fontsize={24} />
-                    </Link>
+                    <Logo fontsize={24} />
                     <MenuButton onClick={isDrawerOpen ? closeDrawer : openDrawer} isActive={isDrawerOpen} />
                     <MobileDrawer isOpen={isDrawerOpen} closeDrawer={closeDrawer} userName={userName} />
                 </Container>
