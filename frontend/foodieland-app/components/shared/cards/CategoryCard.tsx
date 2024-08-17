3e3b258309453dc21a05c3389709ccd973012d15
skmkqw <tkorsakov77@gmail.com>
2024-08-17 19:53:29 +0200

    CategoryProps

--- frontend/foodieland-app/components/shared/cards/CategoryCard.tsx
+++ frontend/foodieland-app/components/shared/cards/CategoryCard.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 
-export default function CategoryCard({ imagePath, name, gradientColors }: {
+interface CategoryProps {
     imagePath: string,
     name: string,
     gradientColors: string
-}) {
+}
+
+export default function CategoryCard({ imagePath, name, gradientColors }: CategoryProps) {
     return (
         <div
             className={`rounded-2xl flex flex-col items-center justify-center gap-6 py-8 px-2 cursor-pointer transition-all duration-200 hover:scale-110 bg-gradient-to-b ${gradientColors}`}>
