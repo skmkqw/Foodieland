5167cc3344da840f6bd6f65cc712378fce26ecdc
skmkqw <tkorsakov77@gmail.com>
2024-08-17 19:38:59 +0200

    Recipe type

--- frontend/foodieland-app/types.ts
+++ frontend/foodieland-app/types.ts
@@ -1,4 +1,7 @@
 import { z } from "zod";
 import { featuredRecipeSchema } from "@/schemas/featuredRecipe";
+import { recipeSchema } from "@/schemas/recipe";
 
 export type FeaturedRecipe = z.infer<typeof featuredRecipeSchema>;
+
+export type Recipe = z.infer<typeof recipeSchema>;
