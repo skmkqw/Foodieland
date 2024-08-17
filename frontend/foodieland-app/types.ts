0bedfda0f3a212bdfc043063e5a35b01cada958d
skmkqw <tkorsakov77@gmail.com>
2024-08-17 20:53:27 +0200

    new types

--- frontend/foodieland-app/types.ts
+++ frontend/foodieland-app/types.ts
@@ -6,4 +6,38 @@ type FeaturedRecipe = z.infer<typeof featuredRecipeSchema>;
 
 type RecipeShort = z.infer<typeof recipeSchema>;
 
-export type Recipe = z.infer<typeof recipeSchema>;
+type Recipe = {
+    name: string,
+    description: string,
+    creationDate: string,
+    category: string,
+    timeToCook: number,
+    imageData: string | null
+}
+
+type RecipeExtended = {
+    recipe: Recipe,
+    creator: RecipeCreator,
+    nutritionInformation: NutritionInformation
+}
+
+type NutritionInformation = {
+    calories: number,
+    protein: number,
+    fat: number,
+    carbohydrate: number,
+    cholesterol: number
+}
+
+type RecipeCreator = {
+    creatorName: string,
+    userImage: string | null
+}
+
+export {
+    RecipeShort,
+    FeaturedRecipe,
+    RecipeExtended,
+    RecipeCreator,
+    NutritionInformation,
+}
