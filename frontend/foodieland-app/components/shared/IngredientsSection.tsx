8d6c12555722eec8da7ceb6b1305c8675d700d4d
skmkqw <tkorsakov77@gmail.com>
2024-08-18 00:27:51 +0200

    update IngredientsSection

--- frontend/foodieland-app/components/shared/IngredientsSection.tsx
+++ frontend/foodieland-app/components/shared/IngredientsSection.tsx
@@ -1,10 +1,10 @@
-import { Container, IngredientList } from "@/components";
+import { Container, IngredientList, Title } from "@/components";
 import { Ingredient } from "@/types";
 
 export default function IngredientsSection({ ingredients }: { ingredients: Array<Ingredient> }) {
     return (
         <Container className="flex flex-col gap-5 md:gap-8 w-full mt-12 md:mt-20">
-            <h1 className="font-semibold text-3xl md:text-4xl">Ingredients</h1>
+            <Title text="Ingredients" className="text-3xl md:text-4xl" />
             <IngredientList ingredients={ingredients} />
         </Container>
     );
