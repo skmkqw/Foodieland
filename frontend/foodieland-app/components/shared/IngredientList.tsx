2b1bf50e84c29a0c3321d569415967f550711be7
skmkqw <tkorsakov77@gmail.com>
2024-08-17 23:58:37 +0200

    IngredientList

--- frontend/foodieland-app/components/shared/IngredientList.tsx
+++ frontend/foodieland-app/components/shared/IngredientList.tsx
@@ -1,9 +1,12 @@
-import { Ingredient } from "@/components";
+import { IngredientItem } from "@/components";
+import { Ingredient } from "@/types";
 
-export default function IngredientList() {
+export default function IngredientList({ ingredients }: { ingredients: Array<Ingredient> }) {
     return (
-        <div>
-            <Ingredient />
+        <div className="flex flex-col w-full">
+            {ingredients.map((item: Ingredient, idx) => (
+                <IngredientItem key={idx} ingredient={item} />
+            ))}
         </div>
     );
 }
