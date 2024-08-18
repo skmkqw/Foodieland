07acb53cfecc82f12ee18b76a28b1e59f9d61f49
skmkqw <tkorsakov77@gmail.com>
2024-08-18 20:08:12 +0200

    updated index

--- frontend/foodieland-app/components/index.ts
+++ frontend/foodieland-app/components/index.ts
@@ -3,7 +3,6 @@ import Logo from "@/components/shared/Logo";
 import Button from "@/components/ui/Button";
 import Inbox from "@/components/shared/Inbox";
 import Footer from "@/components/shared/Footer";
-import IngredientItem from "@/components/shared/IngredientItem";
 import LikeButton from "@/components/ui/LikeButton";
 import LinkButton from "@/components/ui/LinkButton";
 import InputGroup from "@/components/ui/InputGroup";
@@ -11,17 +10,20 @@ import UserInfo from "@/components/shared/UserInfo";
 import Container from "@/components/shared/Container";
 import Description from "@/components/ui/Description";
 import NavBar from "@/components/shared/header/NavBar";
-import RecipeInfo from "@/components/shared/RecipeInfo";
 import Header from "@/components/shared/header/Header";
+import RecipeInfo from "@/components/shared/RecipeInfo";
 import CreatorInfo from "@/components/shared/CreatorInfo";
 import NavLinks from "@/components/shared/header/NavLinks";
 import LoginForm from "@/components/shared/forms/LoginForm";
 import Promosection from "@/components/shared/Promosection";
+import DirectionItem from "@/components/shared/DirectionItem";
+import DirectionList from "@/components/shared/DirectionList";
 import RecipeInfoBar from "@/components/shared/RecipeInfoBar";
 import RecipeCard from "@/components/shared/cards/RecipeCard";
 import MenuButton from "@/components/shared/header/MenuButton";
 import RecipesSection from "@/components/shared/RecipesSection";
 import IngredientList from "@/components/shared/IngredientList";
+import IngredientItem from "@/components/shared/IngredientItem";
 import SocialLinks from "@/components/shared/header/SocialLinks";
 import RegisterForm from "@/components/shared/forms/RegisterForm";
 import CategoryCard from "@/components/shared/cards/CategoryCard";
@@ -31,8 +33,8 @@ import RecipeCardSkeleton from "@/components/shared/RecipeCardSkeleton";
 import FeaturedSlider from "@/components/shared/featured/FeaturedSlider";
 import FeaturedSection from "@/components/shared/featured/FeaturedSection";
 import RecipeSectionSkeleton from "@/components/shared/RecipeSectionSkeleton";
-import { NutritionInformationCard } from "@/components/shared/NutritionInformationCard";
 import FeaturedRecipeCard from "@/components/shared/featured/FeaturedRecipeCard";
+import { NutritionInformationCard } from "@/components/shared/NutritionInformationCard";
 import FeaturedRecipeSkeleton from "@/components/shared/featured/FeaturedRecipeSkeleton";
 
 export {
@@ -61,15 +63,18 @@ export {
     RecipesSection,
     Footer,
     Inbox,
+    DirectionItem,
     IngredientItem,
     CreatorInfo,
     Description,
     RecipeCard,
+    DirectionList,
     RecipeCardSkeleton,
     RecipeInfoBar,
     RecipeSectionSkeleton,
     LikeButton,
     UserInfo,
     RecipeInfo,
-    IngredientList
+    IngredientList,
+    DirectionsSection
 };
