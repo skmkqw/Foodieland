79568bc4fa1735441a5d6964e6f1aea1273fdbe3
skmkqw <tkorsakov77@gmail.com>
2024-08-20 00:37:12 +0200

    removed RecipeContainer slowing down

--- frontend/foodieland-app/components/recipes/RecipeContainer.tsx
+++ frontend/foodieland-app/components/recipes/RecipeContainer.tsx
@@ -4,11 +4,9 @@ import { RecipeExtended } from "@/types";
 import { fetchRecipe } from "@/actions/recipes";
 
 export default async function RecipeContainer({ id }: { id: string }) {
-    
+
     const recipe: RecipeExtended | undefined = await fetchRecipe(id);
-
-    const wait = await new Promise(resolve => setTimeout(resolve, 1000));
-
+    
     if (!recipe) return (
         <Error errorMessage="Oops! An unexpected error occurred while fetching recipe details. Please try again later."
                className="mt-10" />
