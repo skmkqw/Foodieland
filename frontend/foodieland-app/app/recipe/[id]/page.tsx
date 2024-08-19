c51210e280caaed4d2259817adb8d577fb7acfd0
skmkqw <tkorsakov77@gmail.com>
2024-08-20 00:35:02 +0200

    wrapping recipe-related components in Suspense

--- frontend/foodieland-app/app/recipe/[id]/page.tsx
+++ frontend/foodieland-app/app/recipe/[id]/page.tsx
@@ -1,26 +1,15 @@
-import { DirectionsSection, Error, Inbox, IngredientsSection, RecipeInfo } from "@/components";
-import { RecipeExtended } from "@/types";
+import { Inbox, RecipeContainer, RecipePageSkeleton } from "@/components";
+import { Suspense } from "react";
 import { notFound } from "next/navigation";
-import { fetchRecipe } from "@/actions/recipes";
 
 export default async function RecipePage({ params }) {
     if (!params.id) return notFound();
 
-    const recipe:RecipeExtended | undefined = await fetchRecipe(params.id);
-
-    if (!recipe) return (
-        <Error errorMessage="Oops! An unexpected error occurred while fetching recipe details. Please try again later." className="mt-10" />
-    );
-
     return (
         <main className="flex flex-col py-5 sm:py-8 md:py-10">
-            <RecipeInfo
-                recipe={recipe.recipe}
-                creator={recipe.creator}
-                nutritionInformation={recipe.nutritionInformation}
-            />
-            <IngredientsSection ingredients={recipe.ingredients} />
-            <DirectionsSection directions={recipe.directions} />
+            <Suspense fallback={<RecipePageSkeleton />}>
+                <RecipeContainer id={params.id}/>
+            </Suspense>
             <Inbox className="mt-12 md:mt-20 !px-6" />
         </main>
     );
