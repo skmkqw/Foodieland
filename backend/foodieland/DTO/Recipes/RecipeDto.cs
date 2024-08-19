9840b3cf6553b38c048904c75b7a029c7e40a43f
skmkqw <tkorsakov77@gmail.com>
2024-08-19 22:03:11 +0200

    updated RecipeDto

--- backend/foodieland/DTO/Recipes/RecipeDto.cs
+++ backend/foodieland/DTO/Recipes/RecipeDto.cs
@@ -34,28 +34,13 @@ public class RecipeDetailsDto
 
 public class RecipeDto
 {
-    public Guid Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    
-    public string Description { get; set; } = string.Empty;
-    
-    public uint TimeToCook { get; set; }
-    
-    public RecipeCategories Category { get; set; }
+    public RecipeDetailsDto Recipe { get; set; }
+
+    public RecipeCreatorDto Creator { get; set; }
 
     public NutritionInformationDto? NutritionInformation { get; set; }
     
-    public List<CookingDirectionDto>? Directions { get; set; }
-    
     public List<IngredientDto>? Ingredients { get; set; }
     
-    public Guid CreatorId { get; set; }
-    
-    public DateOnly CreationDate { get; set; } 
-    
-    public bool IsPublished { get; set; }
-    
-    public bool IsLiked { get; set; }
-    
-    public string? ImageData { get; set; }
+    public List<CookingDirectionDto>? Directions { get; set; }
 }
