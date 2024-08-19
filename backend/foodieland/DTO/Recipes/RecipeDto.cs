f36235adc42f7ee223b3914f148b31d7a17c459a
skmkqw <tkorsakov77@gmail.com>
2024-08-19 22:24:13 +0200

    updated RecipeCreatorDto

--- backend/foodieland/DTO/Recipes/RecipeDto.cs
+++ backend/foodieland/DTO/Recipes/RecipeDto.cs
@@ -10,6 +10,8 @@ public class RecipeCreatorDto
     public Guid CreatorId { get; set; }
 
     public string CreatorName { get; set; }
+
+    public string? UserImage { get; set; }
 }
 
 public class RecipeDetailsDto
