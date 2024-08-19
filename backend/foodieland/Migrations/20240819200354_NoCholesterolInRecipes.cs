b2a91886e5321b25f69523d2d06e909429599935
skmkqw <tkorsakov77@gmail.com>
2024-08-19 22:04:23 +0200

    applied migrations

---
+++ backend/foodieland/Migrations/20240819200354_NoCholesterolInRecipes.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace foodieland.Migrations
+{
+    /// <inheritdoc />
+    public partial class NoCholesterolInRecipes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cholesterol",
+                table: "NutritionInformation");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Cholesterol",
+                table: "NutritionInformation",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
