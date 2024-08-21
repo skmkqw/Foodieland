ca97c77363026bc3c48b866b2e458712524a3444
skmkqw <tkorsakov77@gmail.com>
2024-08-21 19:43:32 +0200

    included roles in decodeToken method

--- frontend/foodieland-app/lib/authorization.ts
+++ frontend/foodieland-app/lib/authorization.ts
@@ -1,10 +1,17 @@
 import { jwtDecode } from "jwt-decode";
 import { getSession } from "@/lib/session";
 
-export function decodeToken(token: string): { unique_name: string, email: string } {
-    return jwtDecode(token);
-}
+export function decodeToken(token: string): { unique_name: string; email: string; roles: string[] } {
+    const decoded: { unique_name: string; email: string; roles: string | string[] } = jwtDecode(token);
 
+    const roles = Array.isArray(decoded.roles) ? decoded.roles : [decoded.roles];
+
+    return {
+        unique_name: decoded.unique_name,
+        email: decoded.email,
+        roles: roles
+    };
+}
 export async function getUserData() {
     const session = await getSession();
     if (session) return decodeToken(session);
