584e5698f821c0e8ff2be39cda0c7dd6e561aaa6
skmkqw <tkorsakov77@gmail.com>
2024-08-23 13:42:29 +0200

    adjusted root layout

--- frontend/foodieland-app/app/layout.tsx
+++ frontend/foodieland-app/app/layout.tsx
@@ -2,7 +2,6 @@ import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-import { Footer, Header } from "@/components";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
@@ -22,9 +21,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     return (
         <html lang="en">
         <body className={inter.className}>
-        <Header />
         {children}
-        <Footer />
         <Toaster
             position="top-right"
             toastOptions={{
