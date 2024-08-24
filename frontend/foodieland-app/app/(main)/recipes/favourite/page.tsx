899bb3371bfe331621dabd3f55ef10e6b400848d
skmkqw <tkorsakov77@gmail.com>
2024-08-25 01:25:47 +0200

    recipes for favourite page

--- frontend/foodieland-app/app/(main)/recipes/favourite/page.tsx
+++ frontend/foodieland-app/app/(main)/recipes/favourite/page.tsx
@@ -1,4 +1,5 @@
-import { Container, FilterSidebar, Title } from "@/components";
+import { Container, Error, FilterSidebar, RecipeCard, Title } from "@/components";
+import { fetchRecipes } from "@/actions/recipes";
 
 const filterGroups = [
     {
@@ -11,14 +12,27 @@ const filterGroups = [
     }
 ];
 
-export default function FavouritePage() {
+export default async function FavouritePage() {
+    //TODO protect route
+
+    //TODO fetch liked recipes instead of all recipes
+    const recipes = await fetchRecipes(6);
+
+    //TODO responsive design
     return (
         <Container className="w-full py-10">
             <Title text="Recipes that bring happiness to your table ❤️" className="text-4xl" />
-            <div className="grid grid-cols-5 mt-10">
-                <FilterSidebar className="col-span-1" groups={filterGroups}/>
+            <div className="grid grid-cols-5 mt-10 gap-6">
+                <FilterSidebar className="col-span-1" groups={filterGroups} />
                 <div className="col-span-4">
-
+                    {recipes && recipes.length != 0 ?
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+                            {recipes.map((recipe, idx) => (
+                                <RecipeCard recipe={recipe} key={idx} />
+                            ))}
+                        </div>
+                        : <Error errorMessage="Oops! An unexpected error occurred while fetching liked recipes. Please try again later." />
+                    }
                 </div>
             </div>
         </Container>
