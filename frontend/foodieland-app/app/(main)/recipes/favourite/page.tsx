b4b4a6b66eeea4d13910e9351c9a0deb0ff0d8b2
skmkqw <tkorsakov77@gmail.com>
2024-08-25 00:34:31 +0200

    /recipes/favourite route

---
+++ frontend/foodieland-app/app/(main)/recipes/favourite/page.tsx
@@ -0,0 +1,9 @@
+import { Container, Title } from "@/components";
+
+export default function FavouritePage() {
+    return (
+        <Container>
+            <Title text="Recipes that bring happiness to your table" />
+        </Container>
+    );
+}
