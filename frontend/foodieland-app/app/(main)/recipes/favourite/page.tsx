3dd3ec7c80fe36825f479f1506d350b65a524cec
skmkqw <tkorsakov77@gmail.com>
2024-08-25 01:17:31 +0200

    page layout

--- frontend/foodieland-app/app/(main)/recipes/favourite/page.tsx
+++ frontend/foodieland-app/app/(main)/recipes/favourite/page.tsx
@@ -1,9 +1,26 @@
-import { Container, Title } from "@/components";
+import { Container, FilterSidebar, Title } from "@/components";
+
+const filterGroups = [
+    {
+        groupName: "Categories",
+        filterNames: ["Luch", "Dinner", "Vegan", "Seafood"]
+    },
+    {
+        groupName: "Cooking time",
+        filterNames: ["< 5 min", "5-15 min", "15-30 min", "45+ min"]
+    }
+];
 
 export default function FavouritePage() {
     return (
-        <Container>
-            <Title text="Recipes that bring happiness to your table" />
+        <Container className="w-full py-10">
+            <Title text="Recipes that bring happiness to your table ❤️" className="text-4xl" />
+            <div className="grid grid-cols-5 mt-10">
+                <FilterSidebar className="col-span-1" groups={filterGroups}/>
+                <div className="col-span-4">
+
+                </div>
+            </div>
         </Container>
     );
 }
