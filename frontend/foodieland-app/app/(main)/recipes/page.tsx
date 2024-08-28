838fc89d3717baac51e713dba83e5d367b9d9252
skmkqw <tkorsakov77@gmail.com>
2024-08-29 00:11:38 +0200

    recipe page layout

--- frontend/foodieland-app/app/(main)/recipes/page.tsx
+++ frontend/foodieland-app/app/(main)/recipes/page.tsx
@@ -1,9 +1,16 @@
-import { Container } from "@/components";
+import { Container, RecipeGridSkeleton, RecipesPageSection, Title } from "@/components";
+import { Suspense } from "react";
 
-export default function RecipesPage() {
+export default function RecipesPage({ searchParams }) {
     return (
-        <Container>
-            Recipes
+        <Container className="w-full py-10 flex flex-col items-center">
+            <Title
+                text="Explore simple and tasty recipes"
+                className="text-4xl lg:text-start text-center"
+            />
+            <Suspense fallback={<RecipeGridSkeleton />}>
+                <RecipesPageSection searchParams={searchParams} />
+            </Suspense>
         </Container>
     );
 }
