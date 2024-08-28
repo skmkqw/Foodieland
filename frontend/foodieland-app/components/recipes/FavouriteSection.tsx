3ff72b591914a39a162cb4fe47a4e670b9aa9f05
skmkqw <tkorsakov77@gmail.com>
2024-08-28 19:44:02 +0200

    calculating actual pageAmount & displaying errors

--- frontend/foodieland-app/components/recipes/FavouriteSection.tsx
+++ frontend/foodieland-app/components/recipes/FavouriteSection.tsx
@@ -8,15 +8,23 @@ export default async function FavouriteSection({ searchParams }) {
     const nextPage = page + 1;
     const prevPage = page - 1 > 0 ? page - 1 : 1;
 
-    const recipes = await fetchLikedRecipes(page);
-    const categories = recipes ? Array.from(new Set(recipes.map(recipe => recipe.category))) : [];
+    const data = await fetchLikedRecipes(page);
+    if (!data) {
+        return <Error errorMessage="Failed to fetch recipes." />;
+    }
+
+    const { totalAmount, likedRecipes } = data;
+
+    const totalPages = Math.ceil(totalAmount / 4);
+
+    const categories = likedRecipes ? Array.from(new Set(likedRecipes.map(recipe => recipe.category))) : [];
 
     return (
         <div className="flex flex-col items-center">
             <div className="flex flex-col lg:grid lg:grid-cols-5 mt-10 gap-6">
                 <FilterSidebar className="col-span-1" categories={categories} />
-                {recipes && recipes.length != 0 ?
-                    <RecipeGrid recipes={recipes} />
+                {likedRecipes && likedRecipes.length != 0 ?
+                    <RecipeGrid recipes={likedRecipes} />
                     : <Error errorMessage="No recipes found." />
                 }
             </div>
@@ -24,7 +32,7 @@ export default async function FavouriteSection({ searchParams }) {
                 activePage={page}
                 prevPage={prevPage}
                 nextPage={nextPage}
-                totalPages={3}
+                totalPages={totalPages}
                 className="mt-10"
             />
         </div>
