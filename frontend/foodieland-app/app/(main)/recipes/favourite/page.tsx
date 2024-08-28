a51dbef8c5b3ec44136004ae934130df93871379
skmkqw <tkorsakov77@gmail.com>
2024-08-28 17:55:03 +0200

    using pagination

--- frontend/foodieland-app/app/(main)/recipes/favourite/page.tsx
+++ frontend/foodieland-app/app/(main)/recipes/favourite/page.tsx
@@ -1,13 +1,20 @@
-import { Container, Error, FilterSidebar, RecipeGrid, Title } from "@/components";
+import { Container, Error, FilterSidebar, Pagination, RecipeGrid, Title } from "@/components";
 import { fetchLikedRecipes } from "@/actions/recipes";
 import { Suspense } from "react";
 
-export default async function FavouritePage() {
-    const recipes = await fetchLikedRecipes(1);
+export default async function FavouritePage({ searchParams }) {
+
+    let page = parseInt(searchParams.page);
+    page = !page || page < 0 ? 1 : page;
+
+    const nextPage = page + 1;
+    const prevPage = page - 1 > 0 ? page - 1 : 1;
+
+    const recipes = await fetchLikedRecipes(page);
     const categories = recipes ? Array.from(new Set(recipes.map(recipe => recipe.category))) : [];
 
     return (
-        <Container className="w-full py-10">
+        <Container className="w-full py-10 flex flex-col items-center">
             <Title
                 text="Recipes that bring happiness to your table ❤️"
                 className="text-4xl lg:text-start text-center"
@@ -21,6 +28,13 @@ export default async function FavouritePage() {
                     : <Error errorMessage="No recipes found." />
                 }
             </div>
+            <Pagination
+                activePage={page}
+                prevPage={prevPage}
+                nextPage={nextPage}
+                totalPages={3}
+                className="mt-10"
+            />
         </Container>
     );
 }
